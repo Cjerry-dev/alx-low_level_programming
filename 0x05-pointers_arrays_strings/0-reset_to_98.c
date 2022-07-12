feb3ca8b5d6d7f14b7178d971b199bb0d4f45439
C Jerry Iloka <93890891+Cjerry-dev@users.noreply.github.com>
2022-07-12 22:54:54 +0100

    Update 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,7 +3,7 @@
 /*
 *parameter and updates the value it points
 * to to 98.
-*reset_to_98-updates the value of a pointer 
+*reset_to_98-updates the value of a pointer
 */
 void reset_to_98(int *n)
 {
