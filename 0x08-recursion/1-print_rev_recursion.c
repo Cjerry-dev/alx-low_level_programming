508c93692dadfeba50d860ef2a72199bcf0896a1
Cjerry-dev <cjerry.dev@gmail.com>
2022-07-20 20:18:45 -0700

    near miss

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,24 +1,15 @@
 #include "main.h"
 
 /**
- *_puts_recursion - a function that prints a string in reverse
- * @s: string to be reversed , passed into the function as a parameter
- *
- */
-
-void _puts_recursion(char *s)
-
+* _print_rev_recursion -> printing string in reverse using recursion
+* @s: string s
+*/
+void _print_rev_recursion(char *s)
 {
-	int i;
-
-	for (i = 0; s[i] != '\0' ; i++)
-	{
-	}
-	while (i >= 0)
-	{
-	i--;
-	_putchar(s[i]);
-	}
-
+	if (*s == '\0')
+		return;
+	s++;
+	_print_rev_recursion(s);
+	s--;
+	_putchar(*s);
 }
-
--- 0x08-recursion/README.md
+++ 0x08-recursion/README.md
@@ -1,1 +1,1 @@
-welcome to recursion
+# Recursion Problem solutions
